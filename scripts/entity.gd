6f8b813d0f242569c82dc66bd951cae58b288780
Adam J Lee <adamjohnathanlee@gmail.com>
2020-07-11 21:35:37 +0100

    Collision Sorted
    Fixed RayCast2D cast to issue

--- scripts/entity.gd
+++ scripts/entity.gd
@@ -5,7 +5,6 @@ var inputs = {"right": Vector2.RIGHT, "left": Vector2.LEFT, "up": Vector2.UP, "d
 
 func _ready():
 	position = position.snapped(Vector2.ONE * tile_size)
-	position += Vector2.ONE * tile_size/2
 
 func _unhandled_input(event):
 	for dir in inputs.keys():
