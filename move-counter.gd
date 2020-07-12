62e5432a21353352141050e9759b87f4d2f99872
Adam J Lee <adamjohnathanlee@gmail.com>
2020-07-12 01:21:34 +0100

    Move Counter Notes
    Made some quick notes about the move counter.

---
+++ move-counter.gd
@@ -0,0 +1,22 @@
+extends Node
+
+
+export var point_pool = 1
+#this point pool starts at 1 in order to avoid any issues with the 
+
+#func _reduce_point_pool():
+	#if event.is_action_pressed:
+		#point_pool = point_pool - 1
+		
+
+
+
+
+#when game starts add x amount of points to 'point_pool'.
+#every time the user presses a key/does an action (i.e. move, attack, dodge) remove 1 point from the 'point_pool'.
+#when the point pool reaches 0, the state machine takes control of the player character until the round ends.
+
+
+
+
+
